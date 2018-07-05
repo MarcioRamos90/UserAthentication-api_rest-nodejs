9493d344af3811cc282cf1f4cca477c098e5b66f
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-07-05 19:19:28 -0300

    autenticand o usuário retornando um token JWT

--- src/index.js
+++ src/index.js
@@ -7,4 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require("../src/controlles/authcontroler")(app);
+require("../src/controlles/projectcontroller")(app);
+
 app.listen(3000);
